e1797e1235889b0be48f5c6d3d8c8a3879f4c153
fckguapp <adamagardeev@gmail.com>
2025-04-09 14:32:04 +0300

    Updated some files
    added .exe
    updated UI
    added speed test

--- README.md
+++
@@ -1,5 +0,0 @@
-# AutoImageClicker
-Clicker that automatical finding images on your screen and click on it,can be usable for work and automatization in some games
--2 languages
--you can upload your .png images
--you can regulate a interval of clicking and image difference
--- mainCheat.py
+++ mainCheat.py
@@ -4,9 +4,11 @@ import pyautogui
 from tkinter import filedialog
 from tkinter import ttk
 import keyboard
-from PIL import Image, ImageTk
 file_path = ""
 val=0.5
+count = 0
+plusoperate = 1
+
 
 def clicking():
     global file_path
@@ -19,6 +21,8 @@ def clicking():
             while True:
                 pyautogui.click(autof, button="left")
                 time.sleep(click_speed)
+        else:
+            print("Image not found,yet...")
 
 def upload_file():
     global file_path
@@ -30,6 +34,8 @@ def standart_clicker():
     while True:
         mouse_pos = pyautogui.position()
         pyautogui.click(mouse_pos,button="left")
+        clicking_speed = val_speed.get()
+        time.sleep(clicking_speed)
 def get_translation(rus_text, eng_text):
     if language_var.get() == 1:
         return eng_text
@@ -43,27 +49,44 @@ def open_popup():
     label.pack(pady=10)
     close_button = tk.Button(popup, text=get_translation("Закрыть", "Close"), command=popup.destroy)
     close_button.pack(pady=10)
+def open_click_counter():
+    window = tk.Tk()
+    window.title("Счетчик кликов")
+    click_count = 0
+    def on_button_click():
+        nonlocal click_count
+        click_count += 1
+        label.config(text=click_count)
+    label = tk.Label(window, text=click_count)
+    label.pack(pady=10)
+    button_speed = tk.Button(window, text=get_translation("Нажми на меня","Click me!"), command=on_button_click,)
+    button_speed.pack(pady=20)
+
 
 root = tk.Tk()
 root.title("AutoAutoClicker")
-root.geometry("500x500")
+root.geometry("310x520")
+#Init interface
 language_var = tk.IntVar()
-language_checkbox = tk.Checkbutton(root, text="English", variable=language_var, command=lambda: update_interface())
+count = tk.Variable()
+plusoperate = tk.Variable()
+language_checkbox = ttk.Checkbutton(root, text="English", variable=language_var, command=lambda: update_interface())
 language_checkbox.pack(pady=10, anchor="nw")
-button_soveti = tk.Button(root, text=get_translation("Помощь", "Help"), command=open_popup)
+button_soveti = ttk.Button(root, text=get_translation("Помощь", "Help"), command=open_popup)
 button_soveti.pack(pady=20, anchor="n")
-button_start = tk.Button(root, text=get_translation("Запуск", "Start"), command=clicking)
+button_start = ttk.Button(root, text=get_translation("Запуск", "Start"), command=clicking)
 button_start.pack(pady=20, anchor="center")
-button_upload = tk.Button(root, text=get_translation("Загрузка изображения", "Upload Image"), command=upload_file)
+button_upload = ttk.Button(root, text=get_translation("Загрузка изображения", "Upload Image"), command=upload_file)
 button_upload.pack(pady=20, anchor="s")
-val = tk.DoubleVar(value=0.5)
+speed_test = ttk.Button(root,text="Speed test",command=open_click_counter)
+speed_test.pack(anchor="center")
 scale = ttk.Scale(root, orient="horizontal", length=100, from_=0.5, to=1, variable=val)
 scale.pack(anchor="s")
-label_scale = tk.Label(root, text=get_translation("Различие картинки (0,5 to 1)", "Image Difference (0.5 to 1)"), textvariable=val)
+label_scale = ttk.Label(root, text=get_translation("Различие картинки (0,5 to 1)", "Image Difference (0.5 to 1)"), textvariable=val)
 label_scale.pack(pady=20, anchor="center")
 keyboard.add_hotkey('f5', standart_clicker)
-val_speed = tk.DoubleVar(value=0.1)  # Начальная скорость клика
-speed_scale = ttk.Scale(root, orient="horizontal", length=100, from_=0.1, to=1, variable=val_speed)
+val_speed = tk.DoubleVar(value=0.01)  #change it to 0.01 for super mode))
+speed_scale = ttk.Scale(root, orient="horizontal", length=100, from_=0.01, to=1, variable=val_speed)
 speed_scale.pack(pady=10, anchor="s")
 label_speed_scale = ttk.Label(root,text = get_translation("Интервал между кликами", "Interval between clicking"))
 label_speed_scale.pack(pady=20,anchor="s")
@@ -75,5 +98,4 @@ def update_interface():
     label_scale.config(text=get_translation("Различие картинки (0,5 to 1)", "Image Difference (0.5 to 1)"))
     button_soveti.config(text=get_translation("Помощь", "Help"))
     label_speed_scale.config(text=get_translation("Интервал между кликами", "Interval between clicking"))
-
 root.mainloop()
